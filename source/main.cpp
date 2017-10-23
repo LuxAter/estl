14ad3d642d2d4c7f84362fc7e5fc558acd9ffb54
Arden Rasmussen <ardenisthebest@gmail.com>
2017-10-23 16:42:58 -0700

    Initial commit on feature/time

--- source/main.cpp
+++ source/main.cpp
@@ -3,12 +3,10 @@
 #include <iostream>
 
 #include "stream.hpp"
+#include "time.hpp"
 
 int main(int argc, char const* argv[]) {
   float a = 0;
-  // std::cin >> a;
-  estl::cscan("%f", a);
-  estl::cprint("HEX FLOAT: %a\n", a);
-  estl::cscan("%a", a);
-  estl::cprint("FLOAT: %f\n", a);
+  estl::Time t(20001);
+  std::cout << t.Format()
 }
