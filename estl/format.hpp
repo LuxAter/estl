7189ccb7a62adbd91a31a01e149de3280db31f17
Arden Rasmussen <ardenisthebest@gmail.com>
2018-03-06 14:45:10 -0800

    Added abbility to subscript arguments, and completed base aformat

--- estl/format.hpp
+++ estl/format.hpp
@@ -18,12 +18,15 @@
 
 #include "variadic.hpp"
 
+// TODO(Arden): Add compile time breaks for invalid format strings, attempting
+// to subscript a non subscriptable object, ect.
+
 namespace estl {
 namespace format {
 
   namespace {
     template <typename S, typename T>
-    struct is_streamable {
+    struct has_stream {
      private:
       template <typename SS, typename TT>
       static auto check(int)
@@ -35,19 +38,6 @@ namespace format {
      public:
       static constexpr bool value = decltype(check<S, T>(0))::value;
     };
-    // template <typename T, typename I>
-    // struct is_subscriptable {
-    //  private:
-    //   template <typename TT, typename II>
-    //   static auto check(int)
-    //       -> decltype(std::declval<TT>()[std::declval<II>()], std::true_type());
-    //   template <typename>
-    //   static auto check(...) -> std::false_type;
-    //
-    //  public:
-    //   static constexpr bool value = decltype(check<T>(0))::value;
-    // };
-
     template <class T, class Index>
     struct has_subscript_impl {
       template <class T1, class IndexDeduced = Index,
@@ -88,7 +78,6 @@ namespace format {
 
     template <typename TString, typename TChar = typename TString::value_type>
     inline TString to_xstring(std::size_t n, const TChar* fmt, ...) {
-      // XXX Better to use dynamic sizing, not this forced to 256.
       TChar* res;
       if (n != 0) {
         res = static_cast<TChar*>(malloc(sizeof(TChar) * n));
@@ -104,7 +93,7 @@ namespace format {
     }
 
     template <typename T>
-    inline std::string data_fmt(int data[7], std::size_t n,
+    inline std::string data_fmt(int data[8], std::size_t n,
                                 std::string_view type, T args) {
       std::string fmt = "%";
       if (data[3] == 0) fmt += '+';
@@ -118,7 +107,6 @@ namespace format {
       fmt += std::string(type);
       fmt = to_xstring<std::string>(n, fmt.c_str(), args);
       if (data[4] != -1 && data[4] > fmt.size()) {
-        printf("ENTER %d\n", data[2]);
         if (data[1] == -1) {
           data[1] = int(' ');
         }
@@ -127,12 +115,11 @@ namespace format {
         } else if (data[2] == 1) {
           fmt = std::string(data[4] - fmt.size(), data[1]) + fmt;
         } else if (data[2] == 2) {
-          printf("HERE\n");
           if (fmt[0] == '-' || fmt[0] == '+') {
             fmt = fmt[0] + std::string(data[4] - fmt.size(), data[1]) +
                   fmt.substr(1);
           } else {
-            fmt = std::string(data[4] - fmt.size(), data[1]) + fmt;  // TODO
+            fmt = std::string(data[4] - fmt.size(), data[1]) + fmt;
           }
         } else if (data[2] == 3) {
           fmt = std::string(std::floor((data[4] - fmt.size()) / 2.0), data[1]) +
@@ -146,75 +133,130 @@ namespace format {
     template <typename T>
     inline typename std::enable_if<has_format<T, std::string()>::value,
                                    std::string>::type
-    aformat(int data[7], T argument) {
+    aformat(int data[8], T argument) {
+      // TODO(Arden): Implement class format tremplate, and provide requirements
+      // and data.
       return argument.Format();
     }
+
     template <typename T>
-    inline typename std::enable_if<!(has_format<T, std::string()>::value),
-                                   std::string>::type
-    aformat(int data[7], T argument) {
-      std::string fmt = "%";
-      // XXX Utilizing better and more efficient printf format, prefer this if
-      // possilbe. Need to determin edgecases, inorder to allow backup stream
-      // or default type nameing.
-      std::string res;
-      if (std::is_same<T, int>::value) {
-        res = to_xstring<std::string>(4 * sizeof(int), "%d", argument);
+    inline typename std::enable_if<
+        !(has_format<T, std::string()>::value) &&
+            (std::is_same<T, bool>::value || std::is_same<T, char>::value ||
+             std::is_same<T, int>::value || std::is_same<T, unsigned>::value ||
+             std::is_same<T, long>::value ||
+             std::is_same<T, unsigned long>::value ||
+             std::is_same<T, long long>::value ||
+             std::is_same<T, unsigned long long>::value ||
+             std::is_same<T, float>::value || std::is_same<T, double>::value ||
+             std::is_same<T, long double>::value ||
+             std::is_same<T, const char*>::value),
+        std::string>::type
+    aformat(int data[8], T argument) {
+      if (std::is_same<T, bool>::value) {
+        return data_fmt(data, 4 * sizeof(bool), "b", argument);
+      } else if (std::is_same<T, char>::value) {
+        return data_fmt(data, 4 * sizeof(char), "c", argument);
+      } else if (std::is_same<T, int>::value) {
+        return data_fmt(data, 4 * sizeof(int), "d", argument);
       } else if (std::is_same<T, unsigned>::value) {
-        res = to_xstring<std::string>(4 * sizeof(unsigned), "%u", argument);
+        return data_fmt(data, 4 * sizeof(unsigned), "u", argument);
       } else if (std::is_same<T, long>::value) {
-        res = to_xstring<std::string>(4 * sizeof(long), "%ld", argument);
+        return data_fmt(data, 4 * sizeof(long), "ld", argument);
       } else if (std::is_same<T, unsigned long>::value) {
-        res =
-            to_xstring<std::string>(4 * sizeof(unsigned long), "%lu", argument);
+        return data_fmt(data, 4 * sizeof(unsigned long), "lu", argument);
       } else if (std::is_same<T, long long>::value) {
-        res = to_xstring<std::string>(4 * sizeof(long long), "%lld", argument);
+        return data_fmt(data, 4 * sizeof(long long), "lld", argument);
       } else if (std::is_same<T, unsigned long long>::value) {
-        res = to_xstring<std::string>(4 * sizeof(unsigned long long), "%llu",
-                                      argument);
+        return data_fmt(data, 4 * sizeof(unsigned long long), "llu", argument);
       } else if (std::is_same<T, float>::value) {
         const int n = std::numeric_limits<float>::max_exponent10 + 20;
-        res = to_xstring<std::string>(n, "%f", argument);
+        return data_fmt(data, n, "f", argument);
       } else if (std::is_same<T, double>::value) {
         const int n = std::numeric_limits<double>::max_exponent10 + 20;
-        // std::cout << generate_fmt(data, "f") << "\n";
-        res = data_fmt(data, n, "f", argument);
-        // res = to_xstring<std::string>(n, generate_fmt(data, "f").c_str(),
-        //                               argument);
+        return data_fmt(data, n, "f", argument);
       } else if (std::is_same<T, long double>::value) {
         const int n = std::numeric_limits<long double>::max_exponent10 + 20;
-        res = to_xstring<std::string>(n, "%Lf", argument);
+        return data_fmt(data, n, "lf", argument);
       } else if (std::is_same<T, const char*>::value) {
-        res = data_fmt(data, 255, "s", argument);
-        // res = to_xstring<std::string>(0, "%s", argument);
+        return data_fmt(data, 255, "s", argument);
       }
-      return res;
+      return std::string();
+    }
+    template <typename T>
+    inline typename std::enable_if<
+        !(has_format<T, std::string()>::value) &&
+            has_stream<std::stringstream, T>::value &&
+            !(std::is_same<T, bool>::value || std::is_same<T, char>::value ||
+              std::is_same<T, int>::value || std::is_same<T, unsigned>::value ||
+              std::is_same<T, long>::value ||
+              std::is_same<T, unsigned long>::value ||
+              std::is_same<T, long long>::value ||
+              std::is_same<T, unsigned long long>::value ||
+              std::is_same<T, float>::value || std::is_same<T, double>::value ||
+              std::is_same<T, long double>::value ||
+              std::is_same<T, const char*>::value),
+        std::string>::type
+    aformat(int data[8], T argument) {
+      std::stringstream out;
+      // TODO(Arden): Add add flags to stream, such that formatting requirements
+      // are met.
+      out << argument;
+      return out.str();
+    }
+
+    template <typename T>
+    inline
+        typename std::enable_if<has_subscript<T, int>::value, std::string>::type
+        asformat(int data[8], T argument) {
+      if (data[7] == -1) {
+        return aformat(data, argument);
+      } else {
+        return aformat(data, argument[data[7]]);
+      }
+    }
+    template <typename T>
+    inline typename std::enable_if<!(has_subscript<T, int>::value),
+                                   std::string>::type
+    asformat(int data[8], T argument) {
+      return aformat(data, argument);
     }
 
     template <std::size_t I = 0, typename... Args>
     inline typename std::enable_if<I == sizeof...(Args), std::string>::type
-    aiformat(std::size_t index, int data[7], std::tuple<Args...> args) {
+    aiformat(std::size_t index, int data[8], std::tuple<Args...> args) {
       return std::string();
     }
     template <std::size_t I = 0, typename... Args>
         inline typename std::enable_if <
         I<sizeof...(Args), std::string>::type aiformat(
-            std::size_t index, int data[7], std::tuple<Args...> args) {
+            std::size_t index, int data[8], std::tuple<Args...> args) {
       if (index == 0) {
-        return aformat(data, std::get<I>(args));
+        return asformat(data, std::get<I>(args));
       } else {
         return aiformat<I + 1, Args...>(index - 1, data, args);
       }
     }
 
-    bool parse_argument(int data[7], std::string_view fmt) {
-      for (std::size_t i = 0; i < 7; i++) {
+    bool parse_argument(int data[8], std::string_view fmt) {
+      for (std::size_t i = 0; i < 8; i++) {
         data[i] = -1;
       }
       if (fmt.find_first_of(':') != std::string_view::npos) {
         std::string_view index_fmt = fmt.substr(0, fmt.find_first_of(':'));
         if (index_fmt.size() != 0) {
-          data[0] = atoi(index_fmt.begin());
+          if (index_fmt.find_first_of('[') == std::string_view::npos) {
+            data[0] = atoi(index_fmt.begin());
+          } else {
+            std::string_view subscript = index_fmt.substr(
+                index_fmt.find_first_of('[') + 1,
+                index_fmt.size() - index_fmt.find_first_of('[') - 2);
+            index_fmt = index_fmt.substr(0, index_fmt.find_first_of('['));
+            if (index_fmt.size() != 0) {
+              data[0] = atoi(index_fmt.begin());
+            }
+            data[7] = atoi(subscript.begin());
+          }
         }
         fmt = fmt.substr(fmt.find_first_of(':') + 1);
       } else if (fmt.size() != 0) {
@@ -293,12 +335,6 @@ namespace format {
       std::size_t tuple_length = std::tuple_size<std::tuple<Args...>>::value;
       std::string res;
       std::string_view fmt_text = fmt;
-      std::cout << fmt << "\n";
-      for (int i = 0; i < fmt.size(); i++) {
-        std::cout << i % 10;
-      }
-      std::cout << "\n";
-
       while (true) {
         std::string_view arg_fmt, rem_fmt;
         std::size_t pos = fmt_text.find_first_of('{');
@@ -310,9 +346,8 @@ namespace format {
         rem_fmt = fmt_text.substr(pos + 1);
         arg_fmt = fmt_text.substr(pos + 1, rem_fmt.find_first_of('}'));
         fmt_text = rem_fmt.substr(rem_fmt.find_first_of('}') + 1);
-        int data[7];
+        int data[8];
         parse_argument(data, arg_fmt);
-        printf("Item: %d\n", data[0]);
         if (data[0] != -1) {
           res += aiformat(std::size_t(data[0]), data, args);
         } else {
@@ -323,64 +358,7 @@ namespace format {
           }
         }
       }
-      // while (index < fmt.size() || arg_index < tuple_length) {
-      //   std::size_t pos = fmt_text.find_first_of('{');
-      //   if (pos != std::string_view::npos) {
-      //     index += pos + 1;
-      //   } else {
-      //     break;
-      //   }
-      //   std::cout << index << ", " << pos << "\n";
-      //   res += fmt.substr(index - pos - 1, pos);
-      //   std::cout << fmt_text << "<\n";
-      //   fmt_text = fmt.substr(index);
-      //   std::cout << fmt_text << "<<\n";
-      //   std::string_view arg_fmt =
-      //       fmt.substr(index, fmt_text.find_first_of('}'));
-      //   fmt_text = fmt.substr(index + fmt_text.find_first_of('}') + 1);
-      //   std::cout << fmt_text << "<<<\n";
-      //   // std::cout << ">" << arg_fmt << "<\n";
-      //   int data[7];
-      //   parse_argument(data, arg_fmt);
-      //   printf("%d, %d, %d, %d, %d, %d\n", data[0], data[1], data[2],
-      //   data[3],
-      //          data[4], data[5], data[6]);
-      //   // res += std::string(fmt_text.substr(0, index - 1));
-      // }
-      // if(index != fmt.size()){
-      //   // res += fmt.substr(index + fmt_text.find_first_of('}') + 1);
-      // }
       return res;
-      // std::size_t first_open = fmt.find_first_of('{');
-      // std::string_view remain = fmt.substr(first_open);
-      // std::size_t first_close = remain.find_first_of('}');
-      // remain = remain.substr(first_close + 1);
-      // std::string_view argument = fmt.substr(first_open + 1, first_close -
-      // 1); std::string output = std::string(fmt.substr(0, first_open));
-      // std::cout << output << "|" << argument << "|" << remain << "\n";
-      // int data[7];
-      // parse_argument(data, argument);
-      // if (data[0] != -1 &&
-      //     data[0] < std::tuple_size<std::tuple<Args...>>::value) {
-      //   output += aiformat(std::size_t(data[0]), data, args);
-      // } else {
-      //   output += aformat(data, std::get<I>(args));
-      // }
-      // std::string_view remain = fmt.substr(fmt.find_first_of('{') + 1);
-      // std::string_view arg_name = remain.substr(
-      //     0, std::max(remain.find_first_of(':'),
-      //     remain.find_first_of('}')));
-      // std::string_view arg_fmt =
-      //     remain.substr(std::max(std::size_t(0),
-      //     remain.find_first_of(':')),
-      //                   remain.find_first_of('}'));
-      // std::cout << arg_name << ":";
-      // std::cout << arg_fmt << "\n";
-
-      // return std::string(output) +
-      //        std::string(vsformat<I + 1, Args...>(
-      //            remain.substr(remain.find_first_of('}') + 1), args));
-      // return output;
     }
   }  // namespace
 
--- source/main.cpp
+++ source/main.cpp
@@ -9,19 +9,20 @@
 // #include "variadic.hpp"
 // #include "vector.hpp"
 
-class Tester{
-  public:
-    int value;
-    std::string Format(){
-      return "Tester: " + std::to_string(value);
-    }
+class Tester {
+ public:
+  int value;
+  std::string Format() { return "Tester: " + std::to_string(value); }
 };
 
 int main(int argc, char const* argv[]) {
   Tester t;
   t.value = 17;
-  // std::string fmt = estl::format::Format("Hello}{2:17}! This is a {}", "World", "Test", 3.1415);
-  std::string fmt = estl::format::Format("Hello {:-^10} This is more text |{:^ 020.5}| after each replacement", "HI", 3.1415);
+  // std::string fmt = estl::format::Format("Hello}{2:17}! This is a {}",
+  // "World", "Test", 3.1415);
+  std::string fmt = estl::format::Format(
+      "Hello {2[3]:-^10} This is more text |{1[2]:^ 020.5}| after each replacement",
+      "HI", 3.1415, std::string("HELLO"));
   std::cout << "RES \"" << fmt << "\"\n";
   return 0;
 }
