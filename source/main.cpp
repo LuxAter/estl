d95b23208bee31993f5a9e97fd4df8825b4b3505
Arden Rasmussen <ardenisthebest@gmail.com>
2018-05-24 11:30:02 -0700

    Added basic format

--- source/main.cpp
+++ source/main.cpp
@@ -9,19 +9,13 @@
 // #include "stream.hpp"
 // #include "variable.hpp"
 // #include "variadic.hpp"
-#include "vector.hpp"
 
 #include "basic/vector.hpp"
 #include "basic/matrix.hpp"
+#include "basic/format.hpp"
 
 using namespace estl::base;
 
 int main(int argc, char const* argv[]) {
-  Vec3f a(1), b(1.5), c;
-  Mat4f ma({1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 2, 8, 0, 0, 0, 1}), mb, mc;
-  mb.Diagonal(1);
-  // ma.Diagonal(5);
-  std::cout << ma << "," << mb << "\n";
-  mc = Dot(ma, mb);
-  std::cout << mc;
+  std::cout << format("Hello %s", "Arden!") << "\n";
 }
